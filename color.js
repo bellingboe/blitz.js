d7e0c508993f0a501ae81c94504e68b4102305ae
Brenden Ellingboe <bellingboe@corp.acesse.com>
2012-08-12 18:12:59 -0500

    fancy color fading for new items

--- color.js
+++ color.js
@@ -7,6 +7,10 @@ function fadeHint(divId,color) {
 		case "blue":
 		color = "#1DA4ED";
 		break;
+	
+		case "yellow":
+		color = "#fff47d";
+		break;
 
 		default: //if "grey" or some misspelled name (error safe).
 		color = "#ACACAC";
@@ -16,14 +20,14 @@ function fadeHint(divId,color) {
 	//(This example comes from a project which used three main site colors: 
 	//Green, Blue, and Grey)
 
-	$(divId).css("-webkit-transition","all 0.6s ease")
+	$(divId).css("-webkit-transition","all 1.5s ease")
 	.css("backgroundColor","white")
-	.css("-moz-transition","all 0.6s ease")
-	.css("-o-transition","all 0.6s ease")
-	.css("-ms-transition","all 0.6s ease")
+	.css("-moz-transition","all 1.5s ease")
+	.css("-o-transition","all 1.5s ease")
+	.css("-ms-transition","all 1.5s ease")
 	/* Avoiding having to use a jQ plugin. */
 
-	.css("backgroundColor",color).delay(200).queue(function() {
+	.css("backgroundColor",color).delay(500).queue(function() {
 		$(this).css("backgroundColor","white"); 
 		$(this).dequeue(); //Prevents box from holding color with no fadeOut on second click.
 	}); 
